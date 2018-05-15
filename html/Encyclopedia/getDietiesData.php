e3a04be17838b228966f248cf05d6710b2349f9b
Nick <nslang18@gmail.com>
2018-05-15 09:54:00 -0500

    adding new files

---
+++ html/Encyclopedia/getDietiesData.php
@@ -0,0 +1,27 @@
+<?php
+
+
+	$hostname = "127.0.0.1";
+	$username = "root";
+	$password = "chinaboy551";
+	$dbname = "encyclopedia";
+	$sqlQuery = "SELECT * From encyclopedia.dieties";
+
+	//connecting to database
+	$conn = mysqli_connect($hostname, $username, $password, $dbname);
+	if($conn->connect_error){
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$result = mysqli_query($conn, $sqlQuery);
+
+	//store info into an array
+	$data = array();
+	while($row = $result->fetch_assoc()){
+		$data[] = $row;
+	}
+
+	//return data in JSON format
+	echo json_encode($data);
+
+?>
